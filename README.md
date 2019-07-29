f0bab07b4a897c341b2532d3917ad42d4bb44fed
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-07-29 00:35:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# No Hum World
+
+Collecting and validating geo coordinates where [The Hum](https://thehum.info/) is absent.
---
+++ js/index.js
@@ -0,0 +1,78 @@
+addEventListener(
+  'DOMContentLoaded',
+  function () {
+    var zoom = 2;
+    var geolocation = navigator.geolocation;
+    var button = document.querySelector('.button.is-primary');
+    var bar = document.querySelector('.progress');
+    button.addEventListener('click', function () {
+      button.disabled = true;
+      var textContent = button.textContent;
+      button.textContent = getTag('?');
+      var raf = 0;
+      var delay = 10000;
+      var timeout = setTimeout(
+        function () {
+          cancelAnimationFrame(raf);
+          geolocation.clearWatch(watcher);
+          bar.value = delay;
+          button.textContent = textContent;
+          map.flyTo([coordinates[0], coordinates[1]], 12);
+          setTimeout(
+            function () {
+              location.href = [
+                'mailto:andrea.giammarchi@gmail.com?',
+                'subject=' + encodeURIComponent('No Hum World - Coordinates') + '&',
+                'body=' + encodeURIComponent('Another place with no Hum: ' + coordinates)
+              ].join('');
+            },
+            2000);
+        },
+        10000
+      );
+      var time = Date.now();
+      (function progress() {
+        bar.value = Math.min(Date.now() - time, delay);
+        raf = requestAnimationFrame(progress);
+      }());
+      var accuracy = Infinity;
+      var coordinates = [0, 0, 0, 0];
+      var watcher = geolocation.watchPosition(
+        function(position) {
+          var coords = position.coords;
+          var acc = Math.min(coords.accuracy, accuracy);
+          if (acc < accuracy) {
+            accuracy = acc;
+            button.textContent = getTag(acc);
+          }
+          coordinates = [
+            coords.latitude,
+            coords.longitude,
+            coords.altitude || 0,
+            accuracy
+          ];
+          map.flyTo([coords.latitude, coords.longitude], zoom++);
+        },
+        function (error) {
+          console.error(error);
+          cancelAnimationFrame(raf);
+          clearTimeout(timeout);
+          button.textContent = textContent;
+          button.disabled = false;
+        },
+        {
+          enableHighAccuracy: true, 
+          maximumAge        : 0,
+          timeout           : delay * 2
+        }
+      );
+    });
+    var map = L.map('map').setView([51.505, -0.09], zoom++);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+    function getTag(m) {
+      return 'best accuracy: ' + m + ' meters';
+    }
+  }
+);
