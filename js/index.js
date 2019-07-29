09e73263f823e3e2f63b2b3edec0828a55968bec
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-07-29 11:19:11 -0500

    dafuq date

--- js/index.js
+++ js/index.js
@@ -124,7 +124,7 @@ addEventListener(
           'subject',
           'No Hum World - Coordinates @ ' + [
             date.getFullYear(),
-            ('0' + date.getMonth() + 1).slice(-2),
+            ('0' + date.getMonth()).slice(-2),
             ('0' + date.getDate()).slice(-2)
           ].join('-')
         );
